47cc4a5f2f182ab67d86430093ebe0d1d05c3a1a
Matthew Gorski <mgorski8@gmail.com>
2014-10-21 20:58:08 -0400

    Modifies layout page and links

--- SurrealistGames.WebUI/Controllers/HomeController.cs
+++ SurrealistGames.WebUI/Controllers/HomeController.cs
@@ -10,21 +10,8 @@ namespace SurrealistGames.WebUI.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return RedirectToAction("Play", "QuestionGame");
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
