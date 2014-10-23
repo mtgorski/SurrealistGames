54c7aa980110873726fee9badb342f35ae0dc692
Matthew Gorski <mgorski8@gmail.com>
2014-10-22 21:34:43 -0400

    Updates sample data script with sample saved game outcomes

--- SurrealistGames.SQLScripts/CreateSampleData.sql
+++ SurrealistGames.SQLScripts/CreateSampleData.sql
@@ -34,3 +34,25 @@ values
 		(4, 4),
 		(5, 5),
 		(6, 6);
+
+
+insert into AspNetUsers (Id, Email, EmailConfirmed,
+						 PasswordHash, SecurityStamp, UserName,
+						  PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnabled,
+						  AccessFailedCount)
+values ('99ba7c1f-9091-443a-a9d1-78c0ac54b357', 'matt@example.com', 0, 
+		'AHeA2/V7ZzW86cmaitaBcGbGGk8/wsg+jNsGEXOLXAqr//t/u9ToBgBehsrk4fEFUA==',
+		'26812078-4361-48e6-befa-e5b371fe9297', 'matt@example.com', 0, 0, 0, 0)
+
+insert into UserInfo(Id)
+values ('99ba7c1f-9091-443a-a9d1-78c0ac54b357')
+
+declare @UserInfoId int
+select @UserInfoId = UserInfoId 
+	from UserInfo
+	where UserInfo.Id = '99ba7c1f-9091-443a-a9d1-78c0ac54b357'
+
+insert into SavedQuestionGameResult (UserInfoId, QuestionPrefixId, QuestionSuffixId)
+values (@UserInfoId, 2, 1),
+       (@UserInfoId, 5, 3),
+       (@UserInfoId, 2, 4)
