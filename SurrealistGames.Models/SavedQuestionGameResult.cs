171b5123338196869d7f13bd4aa160155171db69
Matthew Gorski <mgorski8@gmail.com>
2014-10-22 21:42:53 -0400

    Adds models for favorites feature

---
+++ SurrealistGames.Models/SavedQuestionGameResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurrealistGames.Models
+{
+    public class SavedQuestionGameResult
+    {
+        public int SavedQuestionGameResultId { get; set; }
+        public int QuestionPrefixId { get; set; }
+        public int QuestionSuffixId { get; set; }
+        public int UserInfoId { get; set; }
+    }
+}
