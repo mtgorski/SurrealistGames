737e8a6d1f638d7effcec9a9cfdd6e3e99e64f56
Matthew Gorski <mgorski8@gmail.com>
2014-10-22 21:09:39 -0400

    Modifies table creation script in preparation for letting users save favorite outcomes

--- SurrealistGames.SQLScripts/CreateTables.sql
+++ SurrealistGames.SQLScripts/CreateTables.sql
@@ -22,3 +22,33 @@ create table RandomQuestionSuffix
 	[RandomQuestionSuffixID] [int] not null primary key,
 	[QuestionSuffixID] [int] null
 )	
+
+create table UserInfo
+(
+	[UserInfoId] [int] not null Identity(1, 1) primary key,
+	[Id] nvarchar(128) not null
+)
+
+create table SavedQuestionGameResult
+(
+	[SavedQuestionId] int Identity(1, 1) primary key,
+	[QuestionPrefixId] int not null,
+	[QuestionSuffixId] int not null,
+	[UserInfoId] int not null
+)
+
+alter table UserInfo
+	add constraint FK_UserInfo_AspNetUsers foreign key (Id)
+	references AspNetUsers (Id) 
+
+alter table SavedQuestionGameResult
+	add constraint FK_SavedQuestionGameResult_QuestionPrefix foreign key (QuestionPrefixId)
+	references QuestionPrefix (QuestionPrefixId)
+
+alter table SavedQuestionGameResult
+	add constraint FK_SavedQuestionGameResult_QuestionSuffix foreign key (QuestionSuffixId)
+	references QuestionSuffix (QuestionSuffixId)
+
+alter table SavedQuestionGameResult
+	add constraint FK_SavedQuestionGameResult_UserInfo foreign key (UserInfoId)
+	references UserInfo (UserInfoId)
