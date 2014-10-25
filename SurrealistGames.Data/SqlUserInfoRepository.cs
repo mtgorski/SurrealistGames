3275b5dc43ea506583db2fefff24e5b166d173c3
Matthew Gorski <mgorski8@gmail.com>
2014-10-25 14:10:02 -0400

    Adds sql repository method to add a user

---
+++ SurrealistGames.Data/SqlUserInfoRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SurrealistGames.Models.Interfaces;
+using SurrealistGames.Repositories;
+
+namespace SurrealistGames.Data
+{
+    public class SqlUserInfoRepository : IUserInfoRepo
+    {
+        public void Add(Models.UserInfo item)
+        {
+            using (var cn = new SqlConnection(Settings.GetConnectionString()))
+            {
+                var cmd = new SqlCommand()
+                {
+                    CommandText = "UserInfo_Insert",
+                    Connection = cn,
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                cmd.Parameters.AddWithValue("@Id", item.Id);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
