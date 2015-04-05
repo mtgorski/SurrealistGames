39f29a885f28956aec9b277924224425ac09f952
mtgorski <mgorski8@gmail.com>
2015-04-05 13:59:25 -0400

    Adds UI for moderation

---
+++ SurrealistGames.WebUI/Controllers/ModerationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SurrealistGames.WebUI.Controllers
+{
+    [Authorize(Roles = "Admin, Moderator")]
+    public class ModerationController : Controller
+    {
+        //
+        // GET: /Moderation/
+        public ActionResult Index()
+        {
+            return View("UnmoderatedContent");
+        }
+	}
+}
